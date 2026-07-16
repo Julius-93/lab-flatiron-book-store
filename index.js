41bcb687f1315c642a35ffc3139d8e0323178a9b
Julius-93 <julius.irungu@student.moringaschool.com>
2026-07-16 21:13:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,53 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStore = {
+    name: 'Flatbooks Technical Books',
+    books: [
+        {
+            id:1,
+            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+            author: 'Marjin Haverbeke',
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+            
+        },
+        {
+            id:2,
+            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
+            author: 'Jon Duckett',
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
+        },
+        {
+            id:3,
+            title: 'JavaScript: The Good Parts',
+            author: 'Douglas Crockford',
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+        },
+        {
+            id:4,
+            title: 'JavaScript: The Definitive Guide',
+            author: 'David Flanagan',
+            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
+            
+        },
+        {
+            id:5,
+            title: 'You Don’t Know JS',
+            author: 'Kyle Simpson',
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
+        },
+        {
+            id:6,
+            title: 'Cracking the Coding Interview',
+            author: 'Gayle Laakmann McDowell',
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
+            
+        }
+    ]
+}
+
+// Write your code here!
+
 // Select the header element
 const bookStoreTitle = document.getElementById("header");
 
@@ -68,10 +115,10 @@ bookStore.books.forEach((book) => {
     bookImage.alt = book.title;
 
     // Add elements to the book container
-    bookContainer.addChild(bookTitle);
-    bookContainer.addChild(bookAuthor);
-    bookContainer.addChild(bookImage);
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
 
     // Add the book container to the book list
-    bookList.addChild(bookContainer);
+    bookList.appendChild(bookContainer);
 });
